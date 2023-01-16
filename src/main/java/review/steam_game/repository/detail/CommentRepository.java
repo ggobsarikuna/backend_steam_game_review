a7236beca1c24e1c4f8bb57439a8f83c4e9cc04b
rylynntruth <yoy159@naver.com>
2023-01-16 14:35:57 +0900

    delete dbconnect
    delete dbconnect

--- src/main/java/review/steam_game/repository/detail/CommentRepository.java
+++ src/main/java/review/steam_game/repository/detail/CommentRepository.java
@@ -2,7 +2,6 @@ package review.steam_game.repository.detail;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import review.steam_game.entity.Comment;
-import review.steam_game.entity.User;
 
 import java.util.Optional;
 
@@ -10,5 +9,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Optional<Comment> findById(Long Id);
 
-    Optional<Comment> findByUserid(String userid);
 }
