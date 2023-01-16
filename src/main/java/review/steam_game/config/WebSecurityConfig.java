bd78398597bc118983939324a4c094571f0da10b
rylynntruth <yoy159@naver.com>
2023-01-16 20:53:49 +0900

    before rds h2 config
    before rds h2 config

--- src/main/java/review/steam_game/config/WebSecurityConfig.java
+++ src/main/java/review/steam_game/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
 
         return (web) -> web.ignoring()
-                //.requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
