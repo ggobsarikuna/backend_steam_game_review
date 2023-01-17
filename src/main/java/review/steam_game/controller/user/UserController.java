86484643c1364bb783a098b064c2656657634db3
ggobsarikuna <hyunwu123@naver.com>
2023-01-17 19:08:25 +0900

    서버 배포 완료

--- src/main/java/review/steam_game/controller/user/UserController.java
+++ src/main/java/review/steam_game/controller/user/UserController.java
@@ -4,10 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import review.steam_game.dto.ResponseDto;
 import review.steam_game.dto.user.UserLoginDto;
 import review.steam_game.dto.user.UserSignUpDto;
@@ -15,7 +12,7 @@ import review.steam_game.service.user.UserService;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 @ApiOperation("User SignUp And Login")
 @Controller
 @RequiredArgsConstructor
